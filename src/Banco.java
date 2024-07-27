c333b2dce32020255b7b645b1f8d177488c227a2
Adriano Silva <frs.adriano@gmail.com>
2024-07-27 13:44:04 -0300

    Refatorando a classe banco e incluindo extrato das contas

--- src/Banco.java
+++ src/Banco.java
@@ -1,16 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
 
 	private String nome;
-	private List<Conta> contas;
+	private List<Conta> contas = new ArrayList<>();
+
+	public Banco(String nome) {
+		this.nome = nome;
+	}
+
+	public void imprimirContas() {
+		System.out.println("=======================");
+		System.out.println("# BANCO #");
+		System.out.println("=======================\n");
+		for (Conta conta: contas) {
+			conta.imprimirExtrato();
+			System.out.println("=======================\n");
+		}
+	}
+
+	public void adicionaConta(Conta conta) {
+		this.contas.add(conta);
+	}
 
 	public String getNome() {
 		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
 	}
 
 	public List<Conta> getContas() {
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,20 @@
 public class Main {
 
 	public static void main(String[] args) {
+		Banco banco = new Banco("Banco do Adriano");
+
 		Cliente cliente = new Cliente("Adriano");
 		
 		Conta cc = new ContaCorrente(cliente);
 		Conta poupanca = new ContaPoupanca(cliente);
 
+		banco.adicionaConta(cc);
+		banco.adicionaConta(poupanca);
+
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
-		
-		cc.imprimirExtrato();
-		poupanca.imprimirExtrato();
+
+		banco.imprimirContas();
 	}
 
 }
